f0dfae9ab4184d3a3449c9b55050a1dc153e4702
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-05-25 08:05:19 +0200

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `YSCN`,
+    title: `NONNAKED`,
     description: `YSCN® Officielt Site | Shop den Nye Kollektion‎`,
     author: `@YSCN`,
     menuLinks: [
