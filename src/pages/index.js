9d6002d9425b075c3fb024fd5d4cda90e73a7a6a
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-02 08:56:35 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import React from "react"
 import SEO from "../components/seo"
 import Parallax from '../components/Hero/Parallax'
 import ShowProduct from '../components/ShowProduct/ShowProduct'
-import InstagramCoverflowEffect from '../components/InstagramCoverflowEffect/InstagramCoverflowEffect'
+// import InstagramCoverflowEffect from '../components/InstagramCoverflowEffect/InstagramCoverflowEffect'
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
     <Parallax />
     <ShowProduct />
-    <InstagramCoverflowEffect />
+    {/* <InstagramCoverflowEffect /> */}
   </>
 )
 
