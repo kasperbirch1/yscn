fab70a2861741ccd985b560c6824de8e8065a561
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-20 11:30:40 +0200

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,14 +31,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: ['Balance', 'BalanceTransaction', 'Product', 'ApplicationFee', 'Sku', 'Subscription'],
-        secretKey: process.env.STRIPE_SECRET_KEY,
-        downloadFiles: true,
-      }
-    },
+    // {
+    //   resolve: `gatsby-source-stripe`,
+    //   options: {
+    //     objects: ['Balance', 'BalanceTransaction', 'Product', 'ApplicationFee', 'Sku', 'Subscription'],
+    //     secretKey: process.env.STRIPE_SECRET_KEY,
+    //     downloadFiles: true,
+    //   }
+    // },
     {
       resolve: `gatsby-source-instagram`,
       options: {
