f8a4cd2dc5112cc7f193d6d287e146043f03b116
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-21 00:15:06 +0200

    updated

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,10 @@
 import PropTypes from "prop-types"
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import DesktopNavMenu from '../components/Menu/DesktopNav'
 import MobileNavMenu from '../components/Menu/MobileNav'
 
 
-
 const Header = ({ siteTitle, menuLinks }) => {
-  const data = useStaticQuery(graphql`
-  query {
-      allContentfulPages(sort: {fields: order, order: ASC}) {
-        nodes {
-          title
-          slug
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <header>
