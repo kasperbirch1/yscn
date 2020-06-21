0bf88e5724985b80b228522976cea97dd976dda5
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-21 11:17:47 +0200

    variable problem fixed

--- src/templates/ContenfullPageTempalate.js
+++ src/templates/ContenfullPageTempalate.js
@@ -1,28 +1,26 @@
 import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-const ContenfullPageTempalate = (props) => {
-    const data = useStaticQuery(graphql`
-    query {
-        contentfulPages(slug: {eq: "about"}) {
-          id
-          title
-          slug
-          content {
-            json
-          }
-        }
-      }
-    `)
-    console.log("ContenfullPageTempalate props:", props.pageContext.slug);
-    // console.log("ContenfullPageTempalate data:", data);
+const ContenfullPageTempalate = ({ data }) => {
     return (
         <>
-            <h1 style={{ paddingTop: '5rem' }}>Hej Tempalte</h1>
             {documentToReactComponents(data.contentfulPages.content.json)}
         </>
     )
 }
 
 export default ContenfullPageTempalate
+
+export const query = graphql`
+query($slug: String!) {
+    contentfulPages(slug: {eq: $slug}) {
+      id
+      title
+      slug
+      content {
+        json
+      }
+    }
+  }
+`
