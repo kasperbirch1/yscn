1e7ba4e463a633336a273d8b257738d11545d2f5
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-06-22 11:40:56 +0200

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FaFacebookSquare, FaInstagram, FaYoutube } from 'react-icons/fa'
 const StyledFooter = styled.footer`
     padding: 1rem;
     background-color: black;
     color: white;
-    text-align: center;
-    text-transform: capitalize;
-    h2 {
-        font-style: italic;
-        margin-bottom: .5rem;
+    display: flex;
+    justify-content: space-around;
+    a {
+        display: block;
+        cursor: pointer;
+        svg {
+            font-size: 2.5rem;
+        }
     }
 `
 
-const Footer = ({ siteSlogan }) => {
+const Footer = () => {
     return (
         <StyledFooter>
-            <h2>{siteSlogan}</h2>
+            <a href="https://www.facebook.com/YSCN.dk" target="_blank">
+                <FaFacebookSquare />
+            </a>
+            <a href="https://www.instagram.com/yscn.dk/" target="_blank">
+                <FaInstagram />
+            </a>
+            <a href="https://www.youtube.com/channel/UCfYbb7qiUwzKxCeQWioH62Q" target="_blank">
+                <FaYoutube />
+            </a>
         </StyledFooter>
     )
 }
